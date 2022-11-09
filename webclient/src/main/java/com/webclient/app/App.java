a339653d1d551a06142fa39fe8de03d691a3000a
Tiago <tiago@pop-os.localdomain>
2022-11-09 14:07:45 +0000

    add eldest student

--- webclient/src/main/java/com/webclient/app/App.java
+++ webclient/src/main/java/com/webclient/app/App.java
@@ -2,6 +2,9 @@ package com.webclient.app;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -16,6 +19,7 @@ public class App {
        writeNumberOfActiveStudents(webClient);
        writeNumberCourses(webClient);
        writelastYearGraduation(webClient);
+       writeEldestStudent(webClient);
 
         try {
             Thread.sleep(2000);
@@ -108,6 +112,32 @@ public class App {
                     System.out.println("An IOException was throwned");
                     e.printStackTrace();
                 }
+            });
+
+    }
+
+    private static void writeEldestStudent(WebClient webClient) {
+        webClient.get().uri("/students").retrieve().bodyToFlux(Student.class)
+            .sort((student1, student2) -> {
+                try {
+                    return new SimpleDateFormat("yyyy-MM-dd").parse(student1.getBirthDate()).compareTo(new SimpleDateFormat("yyyy-MM-dd").parse(student2.getBirthDate()));
+                } catch (ParseException e1) {
+                    System.out.println("An Parse exception was throwned");
+                    e1.printStackTrace();
+                }
+                return 0;
+            })
+            .subscribe(student -> {
+                 try {
+                    FileWriter myWriter = new FileWriter("eldestStudent.txt", true);
+                    myWriter.write("Student's name: " +  student.getName() + " // date: " + student.getBirthDate());
+                    myWriter.write("\n");
+                    myWriter.close();
+                    System.out.println("Eldest Student done");
+                } catch (IOException e) {
+                    System.out.println("An IOException was throwned");
+                    e.printStackTrace();
+                }               
             });
 
     }
